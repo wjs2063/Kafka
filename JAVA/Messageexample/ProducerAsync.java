6a7043616fcb8f69572d8d061b6be1b776c4e8df
pn_jh95 <76778082+wjs2063@users.noreply.github.com>
2022-08-01 15:23:38 +0900

    Update ProducerAsync.java

--- JAVA/Messageexample/ProducerAsync.java
+++ JAVA/Messageexample/ProducerAsync.java
@@ -10,14 +10,14 @@ public class ProducerAsync {
     private static final String TOPIC_NAME = "first-kafka01";
 
     public static void main(String[] args) {
-        Properties props = new Properties(); //Properties 오브젝트를 시작합니다.
+        Properties props = new Properties(); //Properties 오브젝트를 시작
         props.put("bootstrap.servers", "localhost:9092"); //브로커 리스트를 정의합니다. 실제 서버는없고 local에서 진행
         props.put("key.serializer",
-                "org.apache.kafka.common.serialization.StringSerializer"); //메시지 키와 벨류에 문자열을 지정하므로 내장된 StringSerializer를 지정합니다.
+                "org.apache.kafka.common.serialization.StringSerializer"); //메시지 키와 벨류에 문자열을 지정하므로 내장된 StringSerializer를 지정
         props.put("value.serializer",
                 "org.apache.kafka.common.serialization.StringSerializer");
 
-        Producer<String, String> producer = new KafkaProducer<>(props); //Properties 오브젝트를 전달해 새 프로듀서를 생성합니다.
+        Producer<String, String> producer = new KafkaProducer<>(props); //Properties 오브젝트를 전달해 새 프로듀서를 생성
 
         while(true) {
             Scanner sc = new Scanner(System.in);
